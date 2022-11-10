db8f77ea51c620f62e48f7fe344c97bf289d9418
Eric <EricBandur@gmail.com>
2022-11-10 13:02:31 -0600

    schedule functions

--- functions/send-webhook.js
+++ functions/send-webhook.js
@@ -17,45 +17,12 @@ exports.handler = async (event, context) => {
 
   const queueData = queue_data[0];
 
-  // const queueUrl = "https://ciktdhbfjlocsbqtikcn.supabase.co/storage/v1/object/public/public/current-bene-queue.png";
 
+//  if(queueData.devourer_locked) {
 
-  // if queue starts update queue_started to true
-  // if(queueData.number_in_queue > 0 && !queueData.queue_started) {
-  //   await supabase
-  //   .from('benediction-queue')
-  //   .update({ queue_started: true })
-  //   .eq('id', 1)
-  // } 
-
-  // if(queueData.number_in_queue < 1 && queueData.queue_started) {
-  //   await supabase
-  //   .from('benediction-queue')
-  //   .update({ queue_started: false })
-  //   .eq('id', 1)
-  // } 
-
-  // if(queueData.number_in_queue > 0 && !queueData.queue_started) {
-  //   await supabase
-  //   .from('benediction-queue')
-  //   .update({ notify: true })
-  //   .eq('id', 1)
-  // } 
-
-  // if(queueData.number_in_queue > 0 && queueData.queue_started) {
-  //   await supabase
-  //   .from('benediction-queue')
-  //   .update({ notify: false })
-  //   .eq('id', 1)
-  // } 
+//  }
 
 // Handle queue color - default green, greater than 1 orange, more than 999 red
-// let embedColor = 2021216;
-// if (queueData.number_in_queue > 999) {
-//   embedColor = 16711680;
-// } else if (queueData.number_in_queue > 0) {
-//   embedColor = 16753920;
-// }
 let embedColor;
 let date = new Date(queueData.updated_at);
 date = date.toLocaleString("en-US", {timeZone: "America/New_York"});
