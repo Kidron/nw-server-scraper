83b77cccfa2249e0222a195ca20d553711997c9f
Eric <EricBandur@gmail.com>
2022-11-13 10:15:35 -0600

    Update send-webhook.js

--- functions/send-webhook.js
+++ functions/send-webhook.js
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
   const queueData = queue_data[0];
 
 //Handle notify logic
- if(!queueData.devourer_locked && !queueData.notify) {
+ if(queueData.devourer_locked && !queueData.notify) {
   await supabase
   .from('new_world_queue')
   .update({ notify: true })
